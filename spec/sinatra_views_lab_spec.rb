1a16226c5259ef6bee4dbd59f42524f89d62be62
Romario Gustave <romagustave10@gmail.com>
2020-07-02 23:26:36 +0000

    Automatically backed up by Learn

--- spec/sinatra_views_lab_spec.rb
+++ spec/sinatra_views_lab_spec.rb
@@ -40,48 +40,6 @@ describe App do
       expect(last_response.body).to include("The date is")
     end
 
-    it 'includes the current date adescribe App do
-
-  describe 'GET /hello' do
-    before do
-      get '/hello'
-    end
-
-    it 'sends a 200 status code' do
-      expect(last_response.status).to eq(200)
-    end
-
-    it 'renders a template called "hello.erb" ' do
-      expect(last_response.body).to eq(File.read("views/hello.erb"))
-    end
-  end
-
-  describe 'GET /goodbye' do
-    before do
-      get '/goodbye'
-    end
-
-    it 'sends a 200 status code' do
-      expect(last_response.status).to eq(200)
-    end
-
-    it 'renders a template called "goodbye.erb" ' do
-      expect(last_response.body).to include("Goodbye Joe")
-    end
-  end
-  describe 'GET /date' do
-    before do
-      get '/date'
-    end
-
-    it 'sends a 200 status code' do
-      expect(last_response.status).to eq(200)
-    end
-
-    it 'renders a template called "date.erb" ' do
-      expect(last_response.body).to include("The date is")
-    end
-
     it 'includes the current date and time' do
       if last_response.status == 200
         expect(last_response.body).to include(Date.today.strftime("%A, %B %d, %Y"))
@@ -90,13 +48,4 @@ describe App do
       end
     end
   end
-endnd time' do
-      if last_response.status == 200
-        expect(last_response.body).to include(Date.today.strftime("%A, %B %d, %Y"))
-      elsif last_response.status = 404
-        fail "Your application is not responding to GET /date. Did you create that route?"
-      end
-    end
-  end
 end
-
